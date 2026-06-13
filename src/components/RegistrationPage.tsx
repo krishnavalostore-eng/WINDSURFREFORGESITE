a957f1fdfd33b9ae3a8a49fe0d65f49a38fd81d6
krishnavalostore-eng <krishnavalostore@gmail.com>
2026-06-13 21:44:02 +0530

    feat: restore original typewriter & video intro with working assets and inline SVG noise

--- src/components/RegistrationPage.tsx
+++ src/components/RegistrationPage.tsx
@@ -113,7 +113,7 @@ export const RegistrationPage = () => {
       {/* Background Effects */}
       <div className="absolute inset-0 pointer-events-none">
         <div className="absolute top-0 left-0 w-full h-full bg-[radial-gradient(circle_at_center,rgba(37,99,235,0.1)_0%,transparent_70%)]"></div>
-        <div className="absolute inset-0 bg-[url('https://grainy-gradients.vercel.app/noise.svg')] opacity-20"></div>
+        <div className="absolute inset-0 bg-[url('data:image/svg+xml,%3Csvg viewBox=\'0 0 200 200\' xmlns=\'http://www.w3.org/2000/svg\'%3E%3Cfilter id=\'noiseFilter\'%3E%3CfeTurbulence type=\'fractalNoise\' baseFrequency=\'0.65\' numOctaves=\'3\' stitchTiles=\'stitch\'/%3E%3C/filter%3E%3Crect width=\'100%25\' height=\'100%25\' filter=\'url(%23noiseFilter)\'/%3E%3C/svg%3E')] opacity-20"></div>
       </div>
 
       {/* Back to Home Button */}
